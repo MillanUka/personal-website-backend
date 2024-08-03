309de7aaca51c28c7cfecc9f0e1162142c6f41dd
Millan Uka <ukamillan@gmail.com>
2024-08-03 18:26:11 +1200

    Add cors middleware

--- routes/router.go
+++ routes/router.go
@@ -1,15 +1,28 @@
 package routes
 
 import (
+	"time"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"millanuka.com/personal-website-backend/controllers"
 )
 
 func InitRouter() *gin.Engine {
 	router := gin.New()
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"POST"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 	router.Use(gin.Logger())
 	router.Use(gin.Recovery())
 
 	router.POST("/send-user-response", controllers.AddUserResponse)
+
 	return router
 }
